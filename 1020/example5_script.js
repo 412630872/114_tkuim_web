e19b8fd335858ae7d254a8355bd330c13880f075
412630872 <145407974+412630872@users.noreply.github.com>
2025-10-24 16:36:38 +0800

    1020HWK

---
+++ 1020/example5_script.js
@@ -0,0 +1,13 @@
+// example5_script.js
+// 以巢狀 for 產生 1~9 的乘法表
+
+var start = parseInt(prompt("請輸入乘法表的起始數字："));
+var end = parseInt(prompt("請輸入乘法表的結束數字："));
+var output = '';
+for (var i = start; i <= end; i++) {
+  for (var j = 1; j <= 9; j++) {
+    output += i + 'x' + j + '=' + (i * j) + '\t';
+  }
+  output += '\n';
+}
+document.getElementById('result').textContent = output;
