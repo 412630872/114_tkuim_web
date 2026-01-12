d98b90df411984fc865f09898957c10e7fd3394f
412630872 <145407974+412630872@users.noreply.github.com>
2026-01-13 00:18:57 +0800

    更新docker

--- 期末專題/docker/mongo-init.js
+++ 期末專題/docker/mongo-init.js
@@ -6,14 +6,10 @@ db.createUser({
     roles: [{ role: 'readWrite', db: 'cloud_memo' }]
 });
 
-// 1. memos collection
 db.createCollection('memos');
 db.memos.createIndex({ ownerId: 1 });
 
-// 2. users collection + unique email index
 db.createCollection('users');
 db.users.createIndex({ email: 1 }, { unique: true });
 
-// Optional: Pre-create an admin user (password needs to be hashed with bcrypt)
-// I'll skip inserting a user manually to avoid bcrypt complexity here (unless I use a fixed hash).
-// Week12 used a fixed hash.
+
